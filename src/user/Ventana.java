d6fe7df10c9f4e56723a1c35f67d30e72373bea2
BrahiamCastillo <mymoviehb@gmail.com>
2020-05-27 12:51:11 -0400

    Parte superior terminada

--- src/user/Ventana.java
+++ src/user/Ventana.java
@@ -1,9 +1,12 @@
 package user;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,15 +17,16 @@ public class Ventana {
 	JFrame ventana;
 	Border blackline;
 	JPanel panel1, panel2, panel3, panel4, panel5;
-	JLabel uno, dos, tres, cuatro, cinco, seis;
+	JLabel uno, dos, tres, cuatro, cinco, seis, siete, ocho;
 	JTextField campo1, campo2, campo3, campo4, campo5;
-	JButton boton1, boton2;
+	JButton boton1, boton2, boton3;
+	JComboBox<String> combo1;
 	
 	public Ventana() {
 		Estructura();
 	}
 	
-	void ParteIzquierda() {
+	void ParteSuperior() {
 		panel1=new JPanel();
 		blackline=BorderFactory.createLineBorder(Color.black);
 		panel1.setLayout(null);
@@ -60,8 +64,27 @@ public class Ventana {
 		campo5=new JTextField();
 		campo5.setFont(new Font("Times New Roman",Font.BOLD,12));
 		campo5.setBounds(15,212,250,22);
+		siete=new JLabel(" ");
+		siete.setFont(new Font("Times New Roman",Font.CENTER_BASELINE,15));
+		siete.setBorder(blackline);
+		siete.setSize(170,150);
+		siete.setBounds(520,25,180,150);
+		ocho=new JLabel("FOTO");
+		ocho.setFont(new Font("Times New Roman",Font.CENTER_BASELINE,15));
+		ocho.setBounds(585,25,170,150);
+		boton1=new JButton("Cargar foto");
+		boton1.setFont(new Font("Times New Roman",Font.BOLD,14));
+		boton1.setBounds(520,175,180,27);
+		combo1=new JComboBox<String>();
+		combo1.setFont(new Font("Times New Roman",Font.BOLD,12));
+		combo1.addItem("Seleccione");
+		combo1.addItem("Femenino");
+		combo1.addItem("Masculino");
+		combo1.setBounds(520,210,182,27);
 
+		
 
+		
 
 
 
@@ -78,22 +101,38 @@ public class Ventana {
 		panel1.add(campo4);
 		panel1.add(seis);
 		panel1.add(campo5);
+		panel1.add(siete);
+		panel1.add(ocho);
+		panel1.add(boton1);
+		panel1.add(combo1);
 		
 		panel2=new JPanel();
 		panel2.setLayout(null);
 		boton2=new JButton("Probar Conexión");
-		boton2.setFont(new Font("Times New Roman",Font.BOLD,12));
+		boton2.setFont(new Font("Times New Roman",Font.BOLD,14));
 		boton2.setBounds(10,10,230,27);
+		boton3=new JButton("Otro botón");
+		boton3.setFont(new Font("Times New Roman",Font.BOLD,14));
+		boton3.setBounds(510,10,230,27);
+
+
+	
+
+
+
+		
+		
 		panel2.add(boton2);
+		panel2.add(boton3);
 		
 	}
 	
 	void Estructura() {
 		ventana= new JFrame();
 		ventana.setTitle("Formulario de contacto");
-		ventana.setSize(800,600);
+		ventana.setSize(760,680);
 		ventana.setLayout(new BoxLayout(ventana.getContentPane(),BoxLayout.Y_AXIS));
-		ParteIzquierda();
+		ParteSuperior();
 		ventana.add(panel1);
 		ventana.add(panel2);
 		
